e9eb208e7e0b4a77fb0b495ba7baa0ec4a10b4e3
Coty Condry <ccondry@cisco.com>
2022-02-17 15:08:10 -0600

    update multimedia profile and skill to be like skill profile

--- src/contact-center/multimedia-profile.js
+++ src/contact-center/multimedia-profile.js
@@ -5,7 +5,7 @@ module.exports = class MultimediaProfile {
     if (!params.orgId) throw Error('orgId is a required constructor parameter for webex-control-hub/contact-center/multimedia-profile.')
     if (!params.accessToken) throw Error('accessToken is a required constructor parameter for webex-control-hub/contact-center/multimedia-profile.')
     this.params = params
-    this.baseUrl = 'https://api.wxcc-us1.cisco.com'
+    this.baseUrl = `https://config-service.produs1.ciscoccservice.com/cms/api/organization/${this.params.orgId}/multimedia-profile`
   }
 
   /**
@@ -15,7 +15,7 @@ module.exports = class MultimediaProfile {
    */
   async list (page = 0, pageSize = 100) {
     try {
-      const url = `${this.baseUrl}/organization/${this.params.orgId}/multimedia-profile`
+      const url = this.baseUrl
       const options = {
         query: {
           page,
@@ -27,6 +27,35 @@ module.exports = class MultimediaProfile {
       }
       const response = await fetch(url, options)
       return response
+    } catch (e) {
+      throw e
+    }
+  }
+
+  /**
+  * Gets full list of multimedia profiles
+  * @return {Promise} the fetch promise, which resolves to multimedia profiles JSON array when
+  * successful
+  */
+   async listAll () {
+    try {
+      let page = 0
+      let pageSize = 100
+      let atEnd = false
+      const data = []
+      // while item is not found and last page of results has not been reached
+      while (!atEnd) {
+        // keep looking
+        const list = await this.list(page, pageSize)
+        // add results to return data array
+        data.push.apply(data, list)
+        // did we reach the end of the total results?
+        atEnd = list.length < pageSize
+        // increment page for next iteration
+        page++
+      }
+      // return results
+      return data
     } catch (e) {
       throw e
     }
@@ -47,7 +76,6 @@ module.exports = class MultimediaProfile {
       while (!found && !atEnd) {
         // keep looking
         let list = await this.list(page, pageSize)
-        // console.log('found', list.length, 'multimedia profiles')
         // look for item in the current list
         found = list.find(item => item.name === name)
         // did we reach the end of the total results?
@@ -68,7 +96,7 @@ module.exports = class MultimediaProfile {
    * object when successful
    */
   get (id) {
-    const url = `${this.baseUrl}/organization/${this.params.orgId}/multimedia-profile/${id}`
+    const url = `${this.baseUrl}/${id}`
     const options = {
       headers: {
         Authorization: 'Bearer ' + this.params.accessToken
@@ -77,93 +105,41 @@ module.exports = class MultimediaProfile {
     return fetch(url, options)
   }
 
-  // /**
-  //  * Create a multimedia profile
-  //  * @return {Promise} the fetch promise, which resolves to multimedia profile JSON
-  //  * array when successful
-  //  */
-  // create ({
-  //   name,
-  //   multimediaId,
-  //   textValue = '',
-  //   description = '',
-  //   booleanValue = false,
-  //   proficiencyValue = 0
-  // }) {
-  //   try {
-  //     const url = `${this.baseUrl}/organization/${this.params.orgId}/multimedia-profile`
-  //     const options = {
-  //       method: 'POST',
-  //       headers: {
-  //         Authorization: 'Bearer ' + this.params.accessToken
-  //       },
-  //       body: {
-  //         name,
-  //         description,
-  //         organizationId: this.params.orgId,
-  //         activeMultimedias: [{
-  //           textValue,
-  //           booleanValue,
-  //           multimediaId,
-  //           proficiencyValue,
-  //           organizationId: this.params.orgId
-  //         }]
-  //       }
-  //     }
-  //     // console.log(url, options)
-  //     return fetch(url, options)
-  //   } catch (e) {
-  //     throw e
-  //   }
-  // }
-
-  // /**
-  //  * update a multimedia profile using same parameters as create
-  //  * @return {Promise} the fetch promise, which resolves to fetch response body
-  //  */
-  // async update ({
-  //   name,
-  //   multimediaId,
-  //   textValue,
-  //   description,
-  //   booleanValue,
-  //   proficiencyValue
-  // }, existing) {
-  //   try {
-  //     // find existing record if not provided
-  //     if (!existing) {
-  //       existing = await this.find({name})
-  //     }
-  //     if (!existing) {
-  //       throw Error('cannot update multimedia profile - ', name, 'does not exist')
-  //     }
-  //     const url = `${this.baseUrl}/organization/${this.params.orgId}/multimedia-profile/${existing.id}`
-  //     if (name) existing.name = name
-  //     if (description) existing.description = description
-  //     const multimedia = existing.activeMultimedias[0]
-  //     if (multimediaId) multimedia.multimediaId = multimediaId
-  //     if (textValue) multimedia.textValue = textValue
-  //     if (booleanValue) multimedia.booleanValue = booleanValue
-  //     if (proficiencyValue) multimedia.proficiencyValue = proficiencyValue
-  //     const options = {
-  //       method: 'PUT',
-  //       headers: {
-  //         Authorization: 'Bearer ' + this.params.accessToken
-  //       },
-  //       body: existing
-  //     }
-  //     return fetch(url, options)
-  //   } catch (e) {
-  //     throw e
-  //   }
-  // }
+  /**
+   * Create a multimedia profile
+   * @return {Promise} the fetch promise, which resolves to multimedia profile JSON
+   * array when successful
+   */
+   create (body) {
+    try {
+      const url = this.baseUrl
+      const options = {
+        method: 'POST',
+        headers: {
+          Authorization: 'Bearer ' + this.params.accessToken
+        },
+        body
+      }
+      // console.log(url, options)
+      return fetch(url, options)
+    } catch (e) {
+      throw e
+    }
+  }
 
   /**
    * replace a multimedia profile
    * @return {Promise} the fetch promise, which resolves to fetch response body
    */
-  replace (body) {
-    const url = `${this.baseUrl}/organization/${this.params.orgId}/multimedia-profile/${body.id}`
+  update (body) {
+    // console.log('update multimedia profile', body)
+    body.id = body.id || body.multimediaProfileId
+    const url = `${this.baseUrl}/${body.id}`
+    // fix missing body parameters
+    // if (typeof body.version === 'undefined') {
+    //   body.version = 1
+    // }
+    // body.organizationId = this.params.orgId
     const options = {
       method: 'PUT',
       headers: {
@@ -173,38 +149,4 @@ module.exports = class MultimediaProfile {
     }
     return fetch(url, options)
   }
-
-  /**
-   * patch a multimedia profile
-   * @return {Promise} the fetch promise, which resolves to fetch response body
-   */
-  patch (body) {
-    const url = `${this.baseUrl}/organization/${this.params.orgId}/multimedia-profile/${body.id}`
-    const options = {
-      method: 'PATCH',
-      headers: {
-        Authorization: 'Bearer ' + this.params.accessToken,
-        'Content-Type': 'application/merge-patch+json'
-      },
-      body
-    }
-    return fetch(url, options)
-  }
-
-  /**
-   * delete a multimedia profile
-   * @return {Promise} the fetch promise, which resolves to fetch response body
-   */
-  // this does not work, it is disabled on API side with inaccessible foreign-
-  // key constraint
-  // remove (id) {
-  //   const url = `${this.baseUrl}/organization/${this.params.orgId}/multimedia-profile/${id}`
-  //   const options = {
-  //     method: 'DELETE',
-  //     headers: {
-  //       Authorization: 'Bearer ' + this.params.accessToken
-  //     }
-  //   }
-  //   return fetch(url, options)
-  // }
 }
--- src/contact-center/skill.js
+++ src/contact-center/skill.js
@@ -29,167 +29,120 @@ module.exports = class Skill {
   }
 
   /**
+  * Gets full list of skills
+  * @return {Promise} the fetch promise, which resolves to skills JSON array when
+  * successful
+  */
+  async listAll () {
+    try {
+      let page = 0
+      let pageSize = 100
+      let atEnd = false
+      const data = []
+      // while item is not found and last page of results has not been reached
+      while (!atEnd) {
+        // keep looking
+        const list = await this.list(page, pageSize)
+        // add results to return data array
+        data.push.apply(data, list)
+        // did we reach the end of the total results?
+        atEnd = list.length < pageSize
+        // increment page for next iteration
+        page++
+      }
+      // return results
+      return data
+    } catch (e) {
+      throw e
+    }
+  }
+
+  /**
    * Find skill by name
    * @return {Promise} the fetch promise, which resolves to skill JSON
    * object when successful or null if not found
    */
-     async find ({name}) {
-      try {
-        let found = null
-        let page = 0
-        let pageSize = 100
-        let atEnd = false
-        // while item is not found and last page of results has not been reached
-        while (!found && !atEnd) {
-          // keep looking
-          let list = await this.list(page, pageSize)
-          // look for item in the current list
-          found = list.find(item => item.name === name)
-          // did we reach the end of the total results?
-          atEnd = list.length < pageSize
-          // increment page for next iteration
-          page++
-        }
-        // return result
-        return found
-      } catch (e) {
-        throw e
+  async find ({name}) {
+    try {
+      let found = null
+      let page = 0
+      let pageSize = 100
+      let atEnd = false
+      // while item is not found and last page of results has not been reached
+      while (!found && !atEnd) {
+        // keep looking
+        let list = await this.list(page, pageSize)
+        // look for item in the current list
+        found = list.find(item => item.name === name)
+        // did we reach the end of the total results?
+        atEnd = list.length < pageSize
+        // increment page for next iteration
+        page++
+      }
+      // return result
+      return found
+    } catch (e) {
+      throw e
+    }
+  }
+
+  /**
+   * Gets full data for one skill
+   * @return {Promise} the fetch promise, which resolves to skill JSON
+   * object when successful
+   */
+  get (id) {
+    const url = `${this.baseUrl}/${id}`
+    const options = {
+      headers: {
+        Authorization: 'Bearer ' + this.params.accessToken
       }
     }
+    return fetch(url, options)
+  }
 
-  // /**
-  //  * Gets full data for one virtual assistant
-  //  * @return {Promise} the fetch promise, which resolves to virtual assistant JSON
-  //  * object when successful
-  //  */
-  // get (id) {
-  //   const url = `${this.baseUrl}/config/organization/${this.params.orgId}/botconfig/${id}`
-  //   const options = {
-  //     headers: {
-  //       Authorization: 'Bearer ' + this.params.accessToken
-  //     }
-  //   }
-  //   return fetch(url, options)
-  // }
+  /**
+   * Create a skill
+   * @return {Promise} the fetch promise, which resolves to skill JSON
+   * array when successful
+   */
+  create (body) {
+    try {
+      const url = this.baseUrl
+      const options = {
+        method: 'POST',
+        headers: {
+          Authorization: 'Bearer ' + this.params.accessToken
+        },
+        body
+      }
+      // console.log(url, options)
+      return fetch(url, options)
+    } catch (e) {
+      throw e
+    }
+  }
 
-  // /**
-  //  * Validate a Google credentials JSON file has correct permissions
-  //  * @return {Promise} the fetch promise, which resolves to virtual assistants JSON
-  //  * array when successful
-  //  */
-  // validate (credentials) {
-  //   try {
-  //     const url = `${this.baseUrl}/validation/dialogflow/servicekey`
-  //     const options = {
-  //       method: 'POST',
-  //       headers: {
-  //         Authorization: 'Bearer ' + this.params.accessToken
-  //       },
-  //       query: {
-  //         orgId: this.params.orgId
-  //       },
-  //       body: {
-  //         serviceAccountKey: credentials,
-  //         dialogFlowProjectId: credentials.project_id
-  //       }
-  //     }
-  //     return fetch(url, options)
-  //   } catch (e) {
-  //     throw e
-  //   }
-  // }
-
-  // /**
-  //  * Create a virtual assistant
-  //  * @return {Promise} the fetch promise, which resolves to virtual assistants JSON
-  //  * array when successful
-  //  */
-  // async create ({
-  //   serviceAccountKey,
-  //   name,
-  //   icon = '',
-  //   channels = 'chat,voice'
-  // }) {
-  //   try {
-  //     const url = `${this.baseUrl}/config/organization/${this.params.orgId}/botconfig`
-  //     const body = {
-  //       type: 'dialogflowv2',
-  //       name,
-  //       config: {
-  //         serviceAccountKey,
-  //         keyName: 'cred.json',
-  //         regionId: 'global',
-  //         dialogFlowProjectId: serviceAccountKey.project_id
-  //       },
-  //       // icon: "data:image/png;base64,VBORw0KGgoAAAA...."
-  //       icon,
-  //       channels,
-  //       contextServiceFields: {}
-  //     }
-  //     const options = {
-  //       method: 'POST',
-  //       headers: {
-  //         Authorization: 'Bearer ' + this.params.accessToken
-  //       },
-  //       body
-  //     }
-  //     const response = await fetch(url, options)
-  //     return response
-  //   } catch (e) {
-  //     throw e
-  //   }
-  // }
-
-  // /**
-  //  * validate icon for a virtual assistant
-  //  * @return {Promise} the fetch promise, which resolves to virtual assistants JSON
-  //  * array when successful
-  //  */
-  // async validateIcon (icon) {
-  //   try {
-  //     const url = `${this.baseUrl}/validateIcon`
-  //     const options = {
-  //       method: 'POST',
-  //       headers: {
-  //         Authorization: 'Bearer ' + this.params.accessToken
-  //       },
-  //       query: {
-  //         orgId: this.params.orgId
-  //       },
-  //       // {icon: "data:image/png;base64,VBORw0KGgoAAAA...."}
-  //       body: {icon}
-  //     }
-  //     const response = await fetch(url, options)
-  //     return response
-  //   } catch (e) {
-  //     throw e
-  //   }
-  // }
-
-  // /**
-  //  * modify a virtual assistant
-  //  * @return {Promise} the fetch promise, which resolves to fetch response body
-  //  */
-  // modify (id, body) {
-  //   // fix body ID
-  //   delete body.id
-  //   body.botServicesConfigId = id
-  //   // make sure region ID is set
-  //   body.regionId = body.regionId || 'global'
-  //   // make sure dialogflow project ID is set in the config section
-  //   try {
-  //     body.config.dialogFlowProjectId = body.config.serviceAccountKey.project_id || body.config.dialogFlowProjectId
-  //   } catch (e) {
-  //     // continue?
-  //   }
-  //   const url = `${this.baseUrl}/config/organization/${this.params.orgId}/botconfig/${id}`
-  //   const options = {
-  //     method: 'PUT',
-  //     headers: {
-  //       Authorization: 'Bearer ' + this.params.accessToken
-  //     },
-  //     body
-  //   }
-  //   return fetch(url, options)
-  // }
+  /**
+   * replace a skill
+   * @return {Promise} the fetch promise, which resolves to fetch response body
+   */
+  update (body) {
+    // console.log('update skill', body)
+    body.id = body.id || body.skillId
+    const url = `${this.baseUrl}/${body.id}`
+    // fix missing body parameters
+    // if (typeof body.version === 'undefined') {
+    //   body.version = 1
+    // }
+    // body.organizationId = this.params.orgId
+    const options = {
+      method: 'PUT',
+      headers: {
+        Authorization: 'Bearer ' + this.params.accessToken
+      },
+      body
+    }
+    return fetch(url, options)
+  }
 }

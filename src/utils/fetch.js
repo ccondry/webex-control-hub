4221c3caea09e9db1300d91b80fd078c48e0b752
Coty Condry <ccondry@cisco.com>
2023-01-27 18:34:10 -0600

    don't send "undefined" as a string value for an undefined query parameter

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -5,7 +5,7 @@ function addUrlQueryParams (endpoint, params) {
   let url = endpoint
   if (typeof params === 'object') {
     // append URL query paramenters
-    const keys = Object.keys(params)
+    const keys = Object.keys(params).filter(k => typeof params[k] !== 'undefined')
     for (let i = 0; i < keys.length; i++) {
       const key = keys[i]
       const value = params[key]
